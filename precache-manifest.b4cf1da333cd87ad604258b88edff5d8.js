e67dd2b3a6eb1e6f70130257b69331c6c39512f7
zagladin <imcygnus.1@gmail.com>
2021-05-22 17:32:16 +0300

    Updates

---
+++ precache-manifest.b4cf1da333cd87ad604258b88edff5d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426b576564b7490cf1e90735add41d72",
+    "url": "/weatherCard/index.html"
+  },
+  {
+    "revision": "1b846fc11f67e524398a",
+    "url": "/weatherCard/static/css/main.b9015fff.chunk.css"
+  },
+  {
+    "revision": "0b87af84b355922e8947",
+    "url": "/weatherCard/static/js/2.12cde930.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherCard/static/js/2.12cde930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b846fc11f67e524398a",
+    "url": "/weatherCard/static/js/main.914e9c55.chunk.js"
+  },
+  {
+    "revision": "e480e1fb51996451ee8f",
+    "url": "/weatherCard/static/js/runtime-main.0405df61.js"
+  },
+  {
+    "revision": "2fc9d01e5b8f36610fcc072676612986",
+    "url": "/weatherCard/static/media/Bars.2fc9d01e.svg"
+  }
+]);
